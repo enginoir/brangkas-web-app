783c08950b5ff0bcb105c0d484af4a0a15fb4766
Enginoir <124773538+enginoir@users.noreply.github.com>
2023-10-17 23:04:45 +0700

    Revert "G2J-13"

--- backend/validators/table.js
+++
@@ -1,9 +0,0 @@
-const {check} = require('express-validator')
-
-exports.tableCreateValidator = [
-    check('name')
-        .notEmpty()
-        .withMessage('Name is required')
-        .isString()
-        .withMessage('Name must be a string')
-]
